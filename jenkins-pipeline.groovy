90df964128da41f64dc5d7c1f5ed9e6c07904d37
claudiomarinho <38536571+claudiomarinho@users.noreply.github.com>
2018-10-09 10:58:13 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
 node('maven') {
     stage('Lendo Arquivos') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'propet', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'bfateste', showBuildLogs: 'true')
     }
     stage('Verificando') {
         echo 'dummy verification....'
     }
     stage('Aprovacao Coordenador') {
         input 'Aprovacao Coordenador'
-        openshiftDeploy(deploymentConfig: 'propet')
+        openshiftDeploy(deploymentConfig: 'bfateste')
     }
     stage('Efetuando Deploy') {
        echo 'fake stage...'
